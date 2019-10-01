5ea430f51967cedd5b17be36a540cfd28c655258
jes89 <hiter00@naver.com>
2019-10-01 10:44:56 +0900

    first commit

---
+++ src/main/java/com/jenkins/auto_deploy/JenkinsSampleApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.auto_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSampleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jenkins/auto_deploy/MainController.java
@@ -0,0 +1,15 @@
+package com.jenkins.auto_deploy;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	
+	@RequestMapping("/")
+	public String main () {
+		return "main page"; 
+	}
+	
+	
+}
